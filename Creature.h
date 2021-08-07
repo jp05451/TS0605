270896bc2ffcf9d4cb61641dd3625fb75bf6d1f6
jp05451 <jp05451@yahoo.com.tw>
2021-08-08 04:13:55 +0800

    first commit

---
+++ Creature.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#pragma once
+
+class Creature
+{
+public:
+				Creature(string name);
+				Creature(string name, Creature (string name, Creature base));
+private:
+
+};
---
+++ Diary.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#pragma once
+
+class Diary
+{
+public:
+				static void NewDay(string day);
+private:
+				static string day;
+};
